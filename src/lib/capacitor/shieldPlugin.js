bbcbc549fb5a3cd03d7443394bd1488f3b540226
abdullah00110001 <abdullahusimin1@gmail.com>
2026-04-26 14:13:03 +0600

    update: src/lib/capacitor/shieldPlugin.js,

--- src/lib/capacitor/shieldPlugin.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Shield = void 0;
-var core_1 = require("@capacitor/core");
-exports.Shield = (0, core_1.registerPlugin)('Shield');
-;
